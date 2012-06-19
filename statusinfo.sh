4d661cc8e0cae7a0007e53fc45ac7752f4affd38
Ivan Kanakarakis <ivan.kanak@gmail.com>
2012-06-19 15:18:34 +0300

    statusinfo: new colors and format - white/orange/gray - no separators

--- statusinfo.sh
+++ statusinfo.sh
@@ -13,16 +13,16 @@ for desk; do
         0) d="web" ;; 1) d="dev" ;;
         2) d="foo" ;; 3) d="nil" ;;
     esac
-    [ $c -ne 0 ] && fg="&4" && case "$m" in
+    [ $c -ne 0 ] && fg="&5" && case "$m" in
         0) l="T" ;; 1) l="M" ;; 2) l="B" ;;
         3) l="G" ;; 4) l="#" ;; 5) l="F" ;;
-    esac || fg="&3"
+    esac || fg="&9"
     [ $w -eq 0 ] && w="&8-"; [ $u -ne 0 ] && w="$w&5!"
-    left="$left$fg$d $w &9| "
+    left="$left$fg$d $w &8:: "
 done
 
 # music status
-music="$(mpc current -f "%title% #&4by #&3%artist%")"
+music="$(mpc current -f "%title% #&8by #&9%artist%")"
 if [ -z "$music" ]; then music="[stopped]" mstat="å"
 else
     mstat="$(mpc | sed -rn '2s/\[([[:alpha:]]+)].*/\1/p')"
@@ -41,7 +41,7 @@ fi
 date="$(date +"%a %d/%m %R")" dstat="É"
 
 # right status info
-right="&9| &4$mstat &3$music &9| &4$vstat &3$vol &9| &4$dstat &3$date"
+right="&5$mstat &9$music  &5$vstat &9$vol  &5$dstat &9$date"
 
-printf "&L%s&R%s\n" "$left&9[&2$l&9]" "$right"
+printf "&L%s&R%s\n" "$left&9[ &5$l&9 ]" "$right"
 
