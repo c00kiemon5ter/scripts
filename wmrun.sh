1b27e249087d07d458be693efbff25221b8074d7
Ivan Kanakarakis <ivan.kanak@gmail.com>
2012-07-18 11:23:40 +0300

    wmrun: update comments -- ignore

--- wmrun.sh
+++ wmrun.sh
@@ -4,13 +4,13 @@
 : "${ff:="/tmp/${wm}.fifo"}"
 [ -p "$ff" ] || mkfifo -m 600 "$ff"
 
-# run some_sorta_bar
+# spawn a statusbar
 while read -t 60 -r line || true; do
     echo "$line" | grep -Ex "(([[:digit:]]+:)+[[:digit:]]+ ?)+" 2>/dev/null 1>&2 && prev="$line" || line=
     statusinfo.sh ${line:-$prev}
 done < "$ff" | bar &
 
-# run mopag
+# spawn a pager
 #mopag < "$ff" &
 
 "$wm" > "$ff"
